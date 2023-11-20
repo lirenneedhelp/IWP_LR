1e655f62e5d610bd285451b730568a2db37b197f
lirenneedhelp <kohliren0707@gmail.com>
2023-11-20 20:46:25 +0800

    rwewer

---
+++ IWP_LR/Assets/Scripts/HelperItem/HelperItemInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TNT/NewItem")]
+public class HelperItemInfo : ItemInfo
+{
+    public void Buff()
+    {
+
+
+    }
+}
